b9a680401b95eecf2cc3e1c171268a5d3eda0d8c
MacUser <macuser@MacUsers-Air.lan1>
2019-01-10 05:51:45 -0500

    all problems added

---
+++ src/homework_3/problem_4.java
@@ -0,0 +1,38 @@
+package homework_3;
+import java.util.Scanner;
+public class problem_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner amount = new Scanner(System.in);
+		System.out.println(“Loan Amount:”)
+		String amount = input.nextLine();
+		Scanner years = new Scanner(System.in);
+		System.out.println(“Number Of Years:”)
+		String years = input.nextLine();
+		Scanner interest = new Scanner(System.in);
+		System.out.println(“Annual Interest Rate:”)
+		String interest = input.nextLine();
+		
+		int amount_2 = Integer.parseInt(amount);	
+		int years_2 = Integer.parseInt(years);	
+		int interest_2 = Integer.parseInt(interest);	
+		
+		int months = years * 12
+		monthly_amount = amount * (interest * (1 + interest)^ months/(1 + interest)^ months - 1);
+		System.out.println("Monthly Amount:" + monthly_amount)
+		System.out.println("Total Amount:" + amount)
+
+		int p = 0 
+		While p > months;
+			p += 1
+			
+		interest_rate = interest * amount;
+		principal = monthly_amount - interest_rate;
+		amount = amount - principal;
+		
+		
+
+	}
+
+}
---
+++ src/homework_3/problem_5.java
@@ -0,0 +1,24 @@
+package homework_3;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Random;
+public class problem_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		list choices = new Arraylist<int>();
+
+		choices.add(int 1 = "⚀ (1)");
+		choices.add(int 2 = "⚁ (2)");
+		choices.add(int 3 = "⚂ (3)");
+		choices.add(int 4 = "⚃ (4)");
+		choices.add(int 5 = "⚄ (5)");
+		choices.add(int 6 = "⚅ (6)");
+
+		Random random = new Random();
+			IntStream.range(0, 5).forEach();
+			a -> System.out.println(choices.get(random.nextInt(choices.size()))));
+
+	}
+
+}
