ca020c5c19c9532434e0ed88cce9ec890762e0ec
MacUser <macuser@MacUsers-Air.lan1>
2019-01-22 21:36:55 -0500

    i cant run it

--- src/homework_3/problem_5.java
+++ src/homework_3/problem_5.java
@@ -1,23 +1,18 @@
-package homework_3;
-import java.util.ArrayList;
+package isabella_java_hw3.isabella_java_hw3.src.homework_3;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 import java.util.Random;
 public class problem_5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		list choices = new Arraylist<int>();
 
-		choices.add(int 1 = "⚀ (1)");
-		choices.add(int 2 = "⚁ (2)");
-		choices.add(int 3 = "⚂ (3)");
-		choices.add(int 4 = "⚃ (4)");
-		choices.add(int 5 = "⚄ (5)");
-		choices.add(int 6 = "⚅ (6)");
+		String[] choices = {"⚀ (1)", "⚁ (2)", "⚂ (3)", "⚃ (4)", "⚄ (5)", "⚅ (6)"};
+				
 
 		Random random = new Random();
-			IntStream.range(0, 5).forEach();
-			a -> System.out.println(choices.get(random.nextInt(choices.size()))));
+			IntStream.range(0, 5).forEach(null);
+			System.out.println(choices.length);
 
 	}
 
