2f94212d5fa1d2078dad54dc613651d8e3b076ff
MacUser <macuser@MacUsers-Air.lan1>
2019-01-22 21:27:37 -0500

    there are no more problems with it but i cant run it

--- src/homework_3/problem_2.java
+++ src/homework_3/problem_2.java
@@ -1,36 +1,46 @@
-package homework_3;
+package isabella_java_hw3.isabella_java_hw3.src.homework_3;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class problem_2 {
 	
-	Public static boolean isNumeric(String str)
+	public static boolean isNumeric(String str)
 	{
-		For (char c : str.toCharArray())
+		for (char c : str.toCharArray());
 		{
-			If (!Charachter.isDigit) return False
+			If(!Character.isDigit(0)); return false;
 		}
-		return True}
+	}
 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
 		
-		Scanner wishlist = new Scanner(System.in);
-		System.out.println(“How many items do you want in your wish list?”)
-		String wishlist = input.nextLine();
-		If(isNumeric(wishlist) == True;
-			IntStream.range(0, wishlist).forEach(
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("How many items do you want in your wish list?");
+		String wishlist = scanner.nextLine();
+		int new_wishlist = Integer.parseInt(wishlist);
+		If(isNumeric(wishlist) == true);
+			IntStream.range(0, new_wishlist).forEach(
 				n -> {
-					Scanner items = new Scanner(System.in);
+					Scanner scanner1 = new Scanner(System.in);
 					System.out.println("what is the item");
-					String item = item.nextLine();
+					String item = scanner1.nextLine();
 					
-					Scanner items = new Scanner(System.in);
+					Scanner scanner2 = new Scanner(System.in);
 					System.out.println("how much does it cost?");
-					String item = item.nextLine();
+					String items = scanner2.nextLine();
 				}
 			);
 
+	}
+
+
+	private static void If(boolean b) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
